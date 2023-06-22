ced7df442092dc1ccd3b59e29e9a454c8c46c21c
Kevin Schneider <schneider.kev@outlook.de>
2023-06-22 12:08:39 +0200

    wip: port Badge class

---
+++ src/AnyBadge.NET/Badge.fs
@@ -0,0 +1,23 @@
+﻿namespace AnyBadge.NET
+
+type Badge(
+    Label: string,
+    Value: string,
+    ?FontName: string,
+    ?FontSize: int,
+    ?NumPaddingChars: int,
+    ?NumLabelPaddingChars: float,
+    ?NumValuePaddingChars: float,
+    ?Template: string,
+    ?Style: string,
+    ?ValuePrefix: string,
+    ?ValueSuffix: string,
+    ?Thresholds: Map<float,string>,
+    ?DefaultColor: string,
+    ?UseMaxWhenValueExceeds: bool,
+    ?ValueFormat: string,
+    ?TextColor: string,
+    ?Semver: bool
+) = 
+    member this.Label = Label
+    member this.Value = Value
---
+++ src/AnyBadge.NET/Defaults.fs
@@ -0,0 +1,13 @@
+﻿module Defaults
+
+let DEFAULT_FONT = "DejaVu Sans,Verdana,Geneva,sans-serif"
+
+let DEFAULT_FONT_SIZE= 11
+
+let NUM_PADDING_CHARS = 0.5
+
+let DEFAULT_COLOR= "#4c1"
+
+let DEFAULT_TEXT_COLOR = "#fff"
+
+let MASK_ID_PREFIX = "anybadge_"
